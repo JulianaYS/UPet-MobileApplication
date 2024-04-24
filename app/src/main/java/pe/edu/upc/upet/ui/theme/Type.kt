ad3b81a6f9d4fa4d50588cd9e6d0445e72731068
daniel <u202121440@upc.edu.pe>
2024-04-24 18:47:40 -0500

    chore: Added poppins font

--- app/src/main/java/pe/edu/upc/upet/ui/theme/Type.kt
+++ app/src/main/java/pe/edu/upc/upet/ui/theme/Type.kt
@@ -2,14 +2,27 @@ package pe.edu.upc.upet.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import pe.edu.upc.upet.R
 
 // Set of Material typography styles to start with
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_thin, FontWeight.Thin)
+)
+
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
