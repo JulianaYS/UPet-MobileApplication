7e2ac657676a1937bf7a03fb781985c5a38ac0d6
JulianaYS <u20201b782@upc.edu.pe>
2024-04-25 23:06:23 -0500

    fix(pet): fixed imports

--- app/src/main/java/pe/edu/upc/upet/feature_pet/data/remote/PetServiceFactory.kt
+++ app/src/main/java/pe/edu/upc/upet/feature_pet/data/remote/PetServiceFactory.kt
@@ -1,5 +1,7 @@
 package pe.edu.upc.upet.feature_pet.data.remote
 
+import pe.edu.upc.upet.core_network.RetrofitFactory
+
 class PetServiceFactory private constructor() {
     companion object {
         fun getPetService(): PetService {
