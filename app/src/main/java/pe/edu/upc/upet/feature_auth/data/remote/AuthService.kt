27f416703411cab56e837ae46980e6196de9ff65
Rimbow002 <enzotrujilloacosta@gmail.com>
2024-05-03 12:56:07 -0500

    fieatÃ: AuthService

---
+++ app/src/main/java/pe/edu/upc/upet/feature_auth/data/remote/AuthService.kt
@@ -0,0 +1,11 @@
+package pe.edu.upc.upet.feature_auth.data.remote
+
+import retrofit2.Callback
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface AuthService {
+
+    
+}
